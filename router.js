b977b0813a5b4dab072340cfebed575dcca64e58
Kelvin Watson <watsokel@oregonstate.edu>
2016-02-08 18:26:52 -0800

    reuse hierarchy almost complete

--- router.js
+++ router.js
@@ -105,12 +105,15 @@ Router.route('/repair/repairItem/:itemName', function(){
     var repairBusinesses = data;
     if (!err) {
       Session.setPersistent('repairBusinesses', data);
+      Session.setPersistent('reuseMap',false);
+      Session.setPersistent('repairMap',true);
       self.render(googleMap,{
         to: 'map', //yield map
         data: function(){
           return { //one or both of selectedItem / selectedBusiness will be undefined
             repairTitle: repairTitle,
-            selectedRepair: selectedRepair, //should be undefined
+            selectedRepair: selectedRepair,
+            selectedReuse: null,
             selectedItem: selectedItem,
             repairBusinesses: repairBusinesses
           };
@@ -122,6 +125,7 @@ Router.route('/repair/repairItem/:itemName', function(){
           return {
             repairTitle: repairTitle,
             selectedRepair: selectedRepair,
+            selectedReuse: null,
             selectedItem: selectedItem,
             repairBusinesses: repairBusinesses
           };
@@ -178,6 +182,68 @@ Router.route('/repair', function(){
   }
 });
 
+
+Router.route('/reuse/reuseCat/:category/reuseItem/:itemName', function(){
+  var self = this;
+  console.log("/reuse/reuseCat/:category/reuseItem/:itemName ROUTE");
+  var selectedReuse = 'Reuse';
+  var selectedCategory = this.params.category;
+  var selectedItem = this.params.itemName;
+  var breadCrumbs = 'android_reuse_item_bread_crumbs';
+  var googleMap = 'android_map'; //show a map of businesses for that item
+  var reuseTitle = 'Businesses that Accept '+selectedItem+' for Reuse/Resale';
+  this.layout('android');
+  this.render(breadCrumbs,{
+    to: 'bread_crumbs',
+    data: function(){
+      return { //one or both of selectedItem / selectedBusiness will be undefined
+        selectedReuse: selectedReuse,
+        selectedCategory: selectedCategory,
+        selectedItem: selectedItem,
+      }
+    }
+  });
+  this.render('blank_template',{
+    to:'main_content'
+  });
+  this.render('loading_template',{
+    to: 'map'
+  });
+  Meteor.call('getReuseBusinesses', selectedItem, function (err, data) {
+    console.log('using NEW REUSE BUSINESSES data');
+    var reuseBusinesses = data;
+    if (!err) {
+      Session.setPersistent('reuseBusinesses', data);
+      Session.setPersistent('reuseMap',true);
+      Session.setPersistent('repairMap',false);
+      self.render(googleMap,{
+        to: 'map', //yield map
+        data: function(){
+          return { //one or both of selectedItem / selectedBusiness will be undefined
+            reuseTitle: reuseTitle,
+            selectedReuse: selectedReuse, //should be undefined
+            selectedRepair: null,
+            selectedItem: selectedItem,
+            reuseBusinesses: reuseBusinesses
+          };
+        }
+      });
+      self.render('android_list_group',{
+        to: 'main_content', //yield main_content
+        data: function() {
+          return {
+            reuseTitle: reuseTitle,
+            selectedReuse: selectedReuse, //should be undefined
+            selectedRepair: null,
+            selectedItem: selectedItem,
+            reuseBusinesses: reuseBusinesses
+          };
+        }
+      });
+    }
+  });
+});
+
 Router.route('/reuse/reuseCat/:category', function(){
   console.log("REUSE CAT ROUTER");
   var self = this;
@@ -195,6 +261,7 @@ Router.route('/reuse/reuseCat/:category', function(){
     var reuseItems = data;
     if (!err) {
       Session.setPersistent('reuseItems', data);
+      Session.setPersistent('reuseCategory',category);
       self.render('android_list_group',{
         to: 'main_content', //yield main_content
         data: function() {
