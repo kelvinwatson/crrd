db1438970300f8b7c1370a76d8d9de76daad686a
Kelvin <watsokel@oregonstate.edu>
2016-02-09 18:31:44 -0800

    checkboxes

--- .wmi/repairbusinesses.php
+++ .wmi/repairbusinesses.php
@@ -78,7 +78,7 @@ if ($mysqli->connect_errno) {
 
 
 <div class="container">
-  <div class="row"> 
+  <div class="row">
     <h1>Administrator Portal</h1>
   </div>
   <div class="row"> <!--START VIEW TABLE ROW -->
@@ -107,19 +107,19 @@ if ($mysqli->connect_errno) {
               WHERE b.type =  'Repair' AND b.name != 'generic_repair_business'"))) {
               echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
             }
-            
+
             if (!$stmt->execute()) {
               echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
             }
-            
+
             if(!$stmt->bind_result($bID,$bT,$bN,$bStr,$bC,$bSta,$bZ,$bP,$bW,$bLat,$bLng,$iN)){
               echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqlii->connect_error;
             }
-            
+
             $prevbN = $prevbStr = $prevbC = $prevbSta = $prevbZ = $prevbP = $prevbW = $prevbLat = $prevbLng = NULL;
             $i=0;
             $arr = array();
-            
+
             while($stmt->fetch()){
               if($prevbN==NULL){ //only executes the first iteration
                 $prevbN = $bN;
@@ -137,7 +137,7 @@ if ($mysqli->connect_errno) {
                 $prevbLat = $bLat;
                 $prevbLng = $bLng;
               } else {
-                echo 
+                echo
                 "<tr><form action=\"https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php#edit\" method=\"post\">
                 <td><input class=\"btn btn-warning\" type=\"submit\" value=\"edit\"><input type=\"hidden\" name=\"repair-business-id\" value=\"".$prevbID."\"></td>
                 <td>".$prevbN."<input type=\"hidden\" name=\"repair-business-name\" value=\"".$prevbN."\"></td>
@@ -147,11 +147,15 @@ if ($mysqli->connect_errno) {
                 <td>".$prevbZ."<input type=\"hidden\" name=\"repair-business-zip\" value=\"".$prevbZ."\"></td>
                 <td>".$prevbP."<input type=\"hidden\" name=\"repair-business-phone\" value=\"".$prevbP."\"></td>
                 <td>".$prevbW."<input type=\"hidden\" name=\"repair-business-website\" value=\"".$prevbW."\"></td>
-                <td><ul>";
+                <td>
+                <ul>";
                 foreach($arr as $v){
                   echo "<li>".$v."</li>";
+                  echo "<input type=\"hidden\" name=\"repair-items-accepted[]\" value=\"".$v."\">";
                 }
-                echo "</ul></td><input type=\"hidden\" name=\"user-action\" value=\"edit-business\"></form></tr>";
+                echo "</ul></td>
+                  <input type=\"hidden\" name=\"user-action\" value=\"edit-business\"></form></tr>";
+
                 unset($arr);
                 $arr = array();
                 $i=0;
@@ -165,7 +169,7 @@ if ($mysqli->connect_errno) {
                 $prevbW = $bW;
                 $prevbLat = $bLat;
                 $prevbLng = $bLng;
-                $arr[$i++] = $iN;                
+                $arr[$i++] = $iN;
               }
             }
             $stmt->close();
@@ -173,66 +177,104 @@ if ($mysqli->connect_errno) {
           </tbody>
       </table>
     </div>
-    
+
   </div> <!--END VIEW TABLE ROW-->
 
   <div id="edit"></div>
 
   <div class="row"><!--EDIT ROW-->
 
-<?php if ($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['user-action'])):?> 
+<?php if ($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['user-action'])):?>
   <?php if($_POST['user-action']=='edit-business'):?>
     <h3 style="padding-top: 70px;">Edit Repair Business</h3>
     <form class="form-horizontal" action="/">
-      
+
       <div class="form-group">
       <label for="bName" class="col-sm-2 control-label">Repair Business Name</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bName" value="<?php echo htmlspecialchars($_POST['repair-business-name']); ?>">
       </div>
       </div>
-      
+
       <div class="form-group">
       <label for="bStreet" class="col-sm-2 control-label">Street</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bStreet" value="<?php echo htmlspecialchars($_POST['repair-business-street']); ?>">
       </div>
       </div>
-      
+
       <div class="form-group">
       <label for="bCity" class="col-sm-2 control-label">City</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bCity" value="<?php echo htmlspecialchars($_POST['repair-business-city']); ?>">
       </div>
       </div>
-      
+
       <div class="form-group">
       <label for="bState" class="col-sm-2 control-label">State</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bState" value="<?php echo htmlspecialchars($_POST['repair-business-state']); ?>">
       </div>
-      </div>  
+      </div>
 
       <div class="form-group">
       <label for="bZip" class="col-sm-2 control-label">Zip code</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bZip" value="<?php echo htmlspecialchars($_POST['repair-business-zip']); ?>">
       </div>
-      </div>    
-      
+      </div>
+
       <div class="form-group">
       <label for="bPhone" class="col-sm-2 control-label">Phone</label>
       <div class="col-sm-10">
         <input type="tel" class="form-control" id="bPhone" value="<?php echo htmlspecialchars($_POST['repair-business-phone']); ?>">
       </div>
-      </div>    
-      
+      </div>
+
       <div class="form-group">
       <label for="bWebsite" class="col-sm-2 control-label">Website</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bWebsite" value="<?php echo htmlspecialchars($_POST['repair-business-website']); ?>">
       </div>
-      </div>  
+      </div>
+
+      <div class="form-group">
+      <label for="bItems" class="col-sm-2 control-label">Items</label>
+      <div class="col-sm-10">
+
+      <ul class="list-unstyled">
+        <?php
+        if (!($stmt = $mysqli->prepare(
+          "SELECT DISTINCT i.id, i.name FROM item i
+          INNER JOIN business_category_item bci ON bci.iid=i.id
+          INNER JOIN category c ON c.id=bci.cid
+          WHERE c.id=16"))) {
+          echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+
+        if (!$stmt->execute()) {
+          echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        if(!$stmt->bind_result($iID, $iN)){
+          echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqlii->connect_error;
+        }
+
+        while($stmt->fetch()){
+          echo
+          "<li>";
+          if (in_array($iN, $_POST['repair-items-accepted'])){
+            echo "<input type=\"checkbox\" id=\"".$iID."\" name=\"repair-item-id\" value=\"".$iID."\" checked>"." ".$iN;
+          } else{
+             echo "<input type=\"checkbox\" id=\"".$iID."\" name=\"repair-item-id\" value=\"".$iID."\">"." ".$iN;
+          }
+          echo "</li>";
+        }
+        $stmt->close();
+
+        ?>
+      </ul>
+
 
     <div class="form-group">
       <div class="col-sm-offset-2 col-sm-10">
@@ -250,59 +292,60 @@ if ($mysqli->connect_errno) {
   <div class="row">
     <h3 style="padding-top: 70px;">Add Repair Business</h3>
     <form class="form-horizontal" action="/">
-      
+
       <div class="form-group">
       <label for="bName" class="col-sm-2 control-label">Repair Business Name</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bName" placeholder="Repair business name">
       </div>
       </div>
-      
+
       <div class="form-group">
       <label for="bStreet" class="col-sm-2 control-label">Street</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bStreet" placeholder="Street address">
       </div>
       </div>
-      
+
       <div class="form-group">
       <label for="bCity" class="col-sm-2 control-label">City</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bCity" placeholder="City">
       </div>
       </div>
-      
+
       <div class="form-group">
       <label for="bState" class="col-sm-2 control-label">State</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bState" placeholder="State">
       </div>
-      </div>  
+      </div>
 
       <div class="form-group">
       <label for="bZip" class="col-sm-2 control-label">Zip code</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bZip" placeholder="Zip code">
       </div>
-      </div>    
-      
+      </div>
+
       <div class="form-group">
       <label for="bPhone" class="col-sm-2 control-label">Phone</label>
       <div class="col-sm-10">
         <input type="tel" class="form-control" id="bPhone" placeholder="Phone number">
       </div>
-      </div>   
+      </div>
 
       <div class="form-group">
       <label for="bWebsite" class="col-sm-2 control-label">Website</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" id="bWebsite" placeholder="Website">
       </div>
-      </div>  
+      </div>
 
       <div class="form-group">
         <label for="bItems" class="col-sm-2 control-label">Items</label>
         <div class="col-sm-10">
+          <ul class="list-unstyled">
           <?php
           if (!($stmt = $mysqli->prepare(
             "SELECT DISTINCT i.id, i.name FROM item i
@@ -311,35 +354,36 @@ if ($mysqli->connect_errno) {
             WHERE c.id=16"))) {
             echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
           }
-          
+
           if (!$stmt->execute()) {
             echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
           }
-          
+
           if(!$stmt->bind_result($iID, $iN)){
             echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqlii->connect_error;
           }
-          
+
           while($stmt->fetch()){
-            echo 
-            "<td>".$iN."<input type=\"checkbox\" id=\"bItems\" placeholder=\"Items\" name=\"repair-item-name\" value=\"".$iN."\"></td>";              
+            echo
+            "<li><input type=\"checkbox\" id=\"".$iID."\" name=\"repair-item-id\" value=\"".$iID."\">"." ".$iN."</li>";
           }
           $stmt->close();
           ?>
+          </ul>
         </div>
       </div>
-          
+
       <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
           <button type="button" class="btn btn-primary" onclick="codeAddress('add'); return false;">Add Business</button>
         </div>
       </div>
-      
+
     </form>
   </div>
-  
-  
-  
+
+
+
 </div> <!-- END CONTAINER -->
 
 <!--SCRIPTS-->
@@ -347,30 +391,30 @@ if ($mysqli->connect_errno) {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="https://maps.google.com/maps/api/js?"></script> 
+<script type="text/javascript" src="https://maps.google.com/maps/api/js?"></script>
 <script type="text/javascript">
-  
+
   Toast.defaults.width='600px';
-  Toast.defaults.displayDuration=7000;        
-  
+  Toast.defaults.displayDuration=7000;
+
   window.onload = function(){
     debugger;
     var queryStr = window.location.search;
     if(queryStr=='?editSuccess=True'){
       Toast.success('Edit Successful!', 'Edit Confirmation');
     } else if(queryStr=='?editSuccess=False'){
-      Toast.error('There was an error in one or more of your inputs!', 'Edit Status');        
+      Toast.error('There was an error in one or more of your inputs!', 'Edit Status');
     } else if(queryStr=='?addSuccess=True'){
       Toast.success('Add Successful!', 'Add Confirmation');
     }else if(queryStr=='?addSuccess=False'){
-      Toast.error('There was an error in one or more of your inputs!', 'Add Status');        
+      Toast.error('There was an error in one or more of your inputs!', 'Add Status');
     }
   }
-  
+
   function codeAddress(action){
     console.log(action);
     var geocoder = new google.maps.Geocoder();
-    var businessId; 
+    var businessId;
     if(action=='edit'){
       businessId = document.getElementById("bId").value;
     }
@@ -378,7 +422,7 @@ if ($mysqli->connect_errno) {
     var street = document.getElementById("bStreet").value;
     var city = document.getElementById("bCity").value;
     var state = document.getElementById("bState").value;
-    
+
     var zipcode = document.getElementById("bZip").value;
     var phone = document.getElementById("bPhone").value;
     var website = document.getElementById("bWebsite").value;
@@ -397,15 +441,15 @@ if ($mysqli->connect_errno) {
       }
     });
   }
-  
+
   function constructRequest(action, businessId, businessName, street, city, state, zipcode, phone, website, latitude, longitude){
     if(window.XMLHttpRequest) httpRequest = new XMLHttpRequest();
     else if(window.ActiveXObject){
-      try { 
+      try {
         httpRequest = new ActiveXObject('Msxml2.XMLHTTP');
       }
       catch(e){
-        try{  
+        try{
           httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
         } catch(e){}
       }
@@ -414,10 +458,10 @@ if ($mysqli->connect_errno) {
       alert('Unable to create XMLHTTP instance.');
       return false;
     }
-    
+
     httpRequest.onreadystatechange = processResponse;
     httpRequest.open('POST','https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/storeRepairBusiness.php',true);
-    httpRequest.setRequestHeader('Content-type','application/x-www-form-urlencoded');    
+    httpRequest.setRequestHeader('Content-type','application/x-www-form-urlencoded');
     var postParams;
     if(latitude==null){
       latitude='';
@@ -425,10 +469,10 @@ if ($mysqli->connect_errno) {
     if(longitude==null){
       longitude='';
     }
-    if(action=='edit'){  
+    if(action=='edit'){
       postParams = 'action='+action+'&business_id='+businessId+'&business_name='+businessName+'&street='+street+'&city='+city+'&state='+state+'&zipcode='+zipcode+'&phone='+phone+'&website='+website+'&lat='+latitude+'&long='+longitude;
     } else if(action=='add'){
-      postParams = 'action='+action+'&business_name='+businessName+'&street='+street+'&city='+city+'&state='+state+'&zipcode='+zipcode+'&phone='+phone+'&website='+website+'&lat='+latitude+'&long='+longitude;  
+      postParams = 'action='+action+'&business_name='+businessName+'&street='+street+'&city='+city+'&state='+state+'&zipcode='+zipcode+'&phone='+phone+'&website='+website+'&lat='+latitude+'&long='+longitude;
     }
     httpRequest.send(postParams);
   }
@@ -441,29 +485,29 @@ if ($mysqli->connect_errno) {
         var obj = JSON.parse(httpRequest.responseText);
         console.log(obj);
         if(obj.httpResponseCode==400){
-          if(obj.response=='editFailure'){ 
-            window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?editSuccess=False"; 
+          if(obj.response=='editFailure'){
+            window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?editSuccess=False";
           }else if (obj.response=='addFailure'){
             window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?addSuccess=False";
           }
         } else{ //obj.httpResponseCode is 200
           if(obj.response=='editSuccess'){
             debugger;
-            window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?editSuccess=True"; 
+            window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?editSuccess=True";
           } else if(obj.response=='addSuccess'){
-            window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?addSuccess=True";   
+            window.location = "https://web.engr.oregonstate.edu/~watsokel/crrd/wmi/repairbusinesses.php?addSuccess=True";
           }
         }
       }else {
         console.log('Problem with the request');
-      
+
       }
     }catch(e){
       console.log('Caught Exception: ' + e);
     }
   }
-</script> 
-  
+</script>
+
 
 </body>
 </html>
